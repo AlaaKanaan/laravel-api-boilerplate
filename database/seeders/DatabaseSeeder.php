4b8043d166a1d3e01ac29407af6b703cd8020251
Alaa Kanaan <akanaan278@gmail.com>
2024-12-25 15:58:04 +0300

    Remove Posts feature and related functionality
    This commit removes the entire Posts feature, including the database migration, model, controller, seeder, routes, and API resource. The associated references in the database seeder and routing files are also cleaned up to reflect this removal. This streamlines the codebase by eliminating unused or deprecated functionality.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,7 @@ class DatabaseSeeder extends Seeder
 
 
         $this->call([
-//            UsersSeeder::class,
-            PostsSeeder::class,
+            UsersSeeder::class,
         ]);
     }
 }
