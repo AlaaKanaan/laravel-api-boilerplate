0360c260969ce1147923d42973ff062880dffe13
Alaa Kanaan <akanaan278@gmail.com>
2024-12-26 18:05:17 +0300

    Remove unused PostController import from main.php
    The PostController import was no longer used in the file and has been removed to clean up the code. This helps improve maintainability and readability of the routes file.

--- routes/Web/main.php
+++ routes/Web/main.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\V1\PostController;
-
 
 Route::get('/user', function (Request $request) {
     return $request->user();
